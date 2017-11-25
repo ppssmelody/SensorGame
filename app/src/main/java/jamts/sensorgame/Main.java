aa5daf693ef92ea859ed2fb72eb6a883cc304cf4
Jamts <t.jamts666@gmail.com>
2017-11-24 23:25:25 -0800

    Initial commit

---
+++ app/src/main/java/jamts/sensorgame/Main.java
@@ -0,0 +1,19 @@
+package jamts.sensorgame;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class Main extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        setContentView(new GamePanel(this));
+    }
+}
